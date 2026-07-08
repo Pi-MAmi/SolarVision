0bf7e22edef7ef91a03155bc0233296de1628466
Mami <you@example.com>
2026-07-08 20:30:40 +0300

    Backend completed - Reader, Parser, Database, Collector and Flask API

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify, render_template
+
+from database.database import Database
+
+app = Flask(__name__)
+
+db = Database()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/api/live")
+def live():
+
+    data = db.last()
+
+    if data is None:
+        return jsonify({"status": "waiting"})
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+
+    app.run(
+        host="0.0.0.0",
+        port=5000,
+        debug=False
+    )
